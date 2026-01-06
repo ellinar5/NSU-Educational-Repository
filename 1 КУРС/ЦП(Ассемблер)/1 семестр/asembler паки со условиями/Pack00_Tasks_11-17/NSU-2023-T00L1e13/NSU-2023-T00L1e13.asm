ea405dbdc96115f4c139a602f0b2299546145b26
ellina <157985958+ellinar5@users.noreply.github.com>
2026-01-06 14:50:55 +0700

    Add files via upload

---
+++ 1 КУРС/ЦП(Ассемблер)/1 семестр/asembler паки со условиями/Pack00_Tasks_11-17/NSU-2023-T00L1e13/NSU-2023-T00L1e13.asm
@@ -0,0 +1,21 @@
+asect  0x00
+	ldi r1,a
+	ld r1,r1
+	ldi r2,b
+	add r1,r2
+	ldi r3,c
+	st r3,r2
+    ldi r0, c    
+    halt         
+
+INPUTS>
+a:
+  dc 0b01101011    
+b:
+  dc 0b11101101  
+ENDINPUTS>
+
+c:
+  ds 1     
+end
+
