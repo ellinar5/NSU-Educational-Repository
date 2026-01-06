6281a1b9640882acc521fcad56fbb3e1debc790e
ellina <157985958+ellinar5@users.noreply.github.com>
2026-01-06 15:51:34 +0700

    Add files via upload

---
+++ 1 КУРС/ЦП(Ассемблер)/1 семестр/asembler паки со условиями/Pack03_Tasks_L1E05-L3E04/NSU-2023-T03L1e05/NSU-2023-T03L1e05.asm
@@ -0,0 +1,32 @@
+asect  0x00
+
+# =================================
+# INSERT YOUR CODE BELOW
+# Executable instructions only
+# No dc or ds pseudo-instructions
+# Do not include a halt instruction: that has been done already (below)
+# ---------------------------------------------------------------------
+
+
+
+
+# =================================
+# LEAVE THIS PART OF THE FILE ALONE
+# Do not change the next two instructions: they must be the last two
+# instructions executed by your program.
+    ldi r0, content  # Loads the start address of your result into r0 for the robot
+    halt             # Brings execution to a halt
+
+# =================================
+# DATA GOES BELOW
+# We have set this up for you, but you will need to test your program by
+# compiling and running it several times with different input data values
+# (different arrays of numbers)
+# ---------------------------------------------------------------------
+
+DATA>
+length:  dc 10          # the length of the array (between 1 and 10)
+content: dc 2, 0, -3, 4, -9, 0, 7, 88, -92, 18
+ENDDATA>
+end
+
