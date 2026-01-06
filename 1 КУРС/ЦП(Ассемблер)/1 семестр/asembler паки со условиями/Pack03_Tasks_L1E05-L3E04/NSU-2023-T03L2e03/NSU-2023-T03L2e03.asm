b695a4b7439fc1314f5f6e0168d0e11d1d1ea090
ellina <157985958+ellinar5@users.noreply.github.com>
2026-01-06 15:55:27 +0700

    Add files via upload

---
+++ 1 КУРС/ЦП(Ассемблер)/1 семестр/asembler паки со условиями/Pack03_Tasks_L1E05-L3E04/NSU-2023-T03L2e03/NSU-2023-T03L2e03.asm
@@ -0,0 +1,29 @@
+asect  0x00
+	ldi r0,s
+	ldi r3,res
+	st r3,r0
+	ldi r1,0x20
+loop:
+	ld r0,r2
+	tst r2
+	bz exit
+	cmp r2,r1
+	beq probel
+	inc r0
+	br loop
+probel:
+	clr r2
+	st r0,r2
+	inc r0
+	inc r3
+	st r3,r0
+	br loop
+exit:
+	halt
+	
+res: ds 9
+inputs>
+s: dc "cats dont like milk",0
+	ds 57
+endinputs>
+end
